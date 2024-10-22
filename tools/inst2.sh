59cbf07d8997639c4cc986b7f05d808549e26c9d
Your Name <you@example.com>
2024-10-22 17:04:34 +0200

    ...

--- tools/inst2.sh
+++ tools/inst2.sh
@@ -70,6 +70,7 @@ apt-get install -y mpg123
 apt-get install -y alsa-utils
 apt-get install -y python3-mido python3-rtmidi python3-pulsectl
 apt-get install -y gnome-shell
+apt-get install -y gnome-backgrounds
 apt-get install -y gedit
 apt-get install -y gnome-terminal
 apt-get install -y gimp
